28b6dcc6aab5792d453acc1409ba98c252a45336
ebussiere <eddy@embtech.ca>
2020-12-30 14:34:33 -0700

    Fix Navbar2

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Navbar.css';
+import '../../App.css';
 
 function Navbar() {
   return (
     <nav id="mesa-navbar" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div>
-        <a class="fab fa-instagram" href="https://www.instagram.com/mesacreativeyyc">.</a>
-        <a class="fab fa-linkedin-in" href="https://www.linkedin.com/in/eleni-christos-37980055/">.</a>
+        <a href="https://www.instagram.com/mesacreativeyyc">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/eleni-christos-37980055/">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
